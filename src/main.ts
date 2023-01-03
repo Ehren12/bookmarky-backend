073db55e01dccfe08a28dea8dcea9ed5932eff09
Ehren Nwokocha <nwokochaehren@gmail.com>
2023-01-03 01:06:22 +0100

    revert cors changes

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import * as passport from 'passport';
 import * as redisConnect from 'connect-redis';
 import * as redis from 'redis';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const RedisStore = redisConnect(session);
   const client = redis.createClient({
     host: process.env.REDIS_HOST,
@@ -18,6 +18,7 @@ async function bootstrap() {
   });
   //security
   app.use(helmet());
+  app.enableCors({ origin: process.env.FRONTEND_ORIGIN, credentials: true });
   // const httpAdapter = app.get(HttpAdapterHost).httpAdapter;
   // const expressApp = httpAdapter.getHttpServer();
   // expressApp.set('trust proxy', 1);
